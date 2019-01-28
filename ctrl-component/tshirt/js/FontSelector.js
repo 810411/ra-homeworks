b238a141b99bd2b117b51095f485da449eff4f16
Grigorii Balabanov <810411@mail.ru>
2019-01-28 12:19:43 +0500

    ctrl-component done

--- ctrl-component/tshirt/js/FontSelector.js
+++ ctrl-component/tshirt/js/FontSelector.js
@@ -1,7 +1,26 @@
 const FontSelector = ({fonts, selectedFont, onSelect}) => {
+  const pictureFontList = fonts.map(({name, path}) => {
     return (
-        <div className="font-picker">
-            Выберите шрифт
-        </div>
+      <div className="grid center font-item">
+        <input
+          type="radio"
+          name="font"
+          value={name}
+          id={name}
+          checked={selectedFont}
+          onChange={event => onSelect(fonts.filter(font => font.name === event.target.value)[0])}
+        />
+        <label htmlFor={name} className="grid-1">
+          <PictureFont path={path} text={name.slice(0, 3)}/>
+        </label>
+      </div>
     )
+  });
+
+  return (
+    <div className="font-picker">
+      {pictureFontList}
+    </div>
+
+  )
 };
