f7e209ede7aa2f9cfc654c8b6900a60be5283e7f
Grigorii Balabanov <810411@mail.ru>
2019-02-18 22:45:30 +0500

    instruments done

--- instruments/mortgage/js/MortgateCalculator.js
+++ instruments/mortgage/js/MortgateCalculator.js
@@ -1,9 +1,20 @@
+const {Row, Col, Form, Select, Input, Button} = window.antd;
+
+const formItemLayout = {
+  labelCol: {span: 8},
+  wrapperCol: {span: 16},
+};
+
 const MortgateCalculator = () => (
-  <div>
-    <form action="">
+  <Row type="flex" justify="center">
+    <Form>
       <Autocomplete/>
       <Inputs/>
-      <Button/>
-    </form>
-  </div>
+      <Col offset={8}>
+        <Button type="default">
+          Отправить
+        </Button>
+      </Col>
+    </Form>
+  </Row>
 );
--- instruments/mortgage/js/Select.js
+++ instruments/mortgage/js/Select.js
@@ -1,10 +1,23 @@
-const Autocomplete = () => (
-  <div>
-    Тип квартиры:<br/>
-    <select>
-      <option>Квартира в новостройке</option>
-      <option>Готовая квартира</option>
-      <option>Загородный дом</option>
-    </select>
-   </div>
-);
+const Autocomplete = () => {
+  const Option = Select.Option;
+
+  return (
+    <Form.Item
+      label="Тип квартиры:"
+      {...formItemLayout}
+    >
+      <Select
+        autoFocus
+        showSearch
+        placeholder="Выберите тип квартиры"
+        optionFilterProp="children"
+        filterOption={(input, option) =>
+          option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+      >
+        <Option value="jack">Квартира в новостройке</Option>
+        <Option value="lucy">Готовая квартира</Option>
+        <Option value="tom">Загородный дом</Option>
+      </Select>
+    </Form.Item>
+  )
+};
